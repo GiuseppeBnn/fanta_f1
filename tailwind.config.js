c4dfdd7665f1489eec06489e1baeb5110e71f54f
GiuseppeBnn <giusbon2@gmail.com>
2023-08-19 13:08:20 +0200

    changed font and style

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
       "content-dark": "#1e1e1e",
       "primary-focus": "#9B0400",
       "secondary-focus": "#343448",
+      "base-100": "#EDEDED",
     }
   },
   plugins: [],
