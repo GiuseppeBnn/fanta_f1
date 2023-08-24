6175dd637d653354c677e7802088b12b985c30ef
GiuseppeBnn <giusbon2@gmail.com>
2023-08-24 12:34:41 +0200

    added team hex colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,18 @@ module.exports = {
       "primary-focus": "#9B0400",
       "secondary-focus": "#343448",
       "base-100": "#EDEDED",
+
+      "mercedes": "#6CD3BF",
+      "redbull": "#3671C6",
+      "ferrari": "#F91536",
+      "mclaren": "#F58020",
+      "alpine": "#2293D1",
+      "alphatauri": "#5E8FAA",
+      "astonmartin": "#358C75",
+      "williams": "#37BEDD",
+      "haas": "#B6BABD",
+      "alfaromeo": "#C92D4B",
+
     }
   },
   plugins: [],
